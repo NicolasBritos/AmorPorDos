85c1f5fbc8432d7e7a01dab969db37af5f4cf367
NicolasBritosYaca <nicolas.britos@Yacare.com>
2022-11-02 16:39:46 -0300

    Se añade el cronometro

--- gotop.js
+++ gotop.js
@@ -38,3 +38,24 @@ document.addEventListener("scroll", () => {
     backToTopButton.classList.add("hidden");
   }
 });
+
+
+/*Cronometro*/
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Feb 02, 2023 19:00:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('dias').innerText = Math.floor(distance / (day)),
+        document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
+
+    }, second)
